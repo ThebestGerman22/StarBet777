dc0ad18d920df3bdae3fb3baa2860c165a8548b4
GABRIEL COIMBRA <GABRIEL COIMBRA@GABRIEL>
2025-03-22 20:20:31 -0300

    Adicionar arquivos de projeto.

---
+++ StarBet777/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace StarBet777
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btSpin = new System.Windows.Forms.Button();
+            this.tmrSpin = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(293, 308);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(370, 308);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 55);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(446, 308);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 55);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Gainsboro;
+            this.label4.Font = new System.Drawing.Font("OCR A Extended", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(140, 9);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label4.Size = new System.Drawing.Size(500, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "STARBET777 CASSINO!!!";
+            // 
+            // btSpin
+            // 
+            this.btSpin.BackColor = System.Drawing.Color.SpringGreen;
+            this.btSpin.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSpin.Location = new System.Drawing.Point(275, 385);
+            this.btSpin.Name = "btSpin";
+            this.btSpin.Size = new System.Drawing.Size(248, 53);
+            this.btSpin.TabIndex = 4;
+            this.btSpin.Text = "CLIQUE AQUI PARA JOGAR!";
+            this.btSpin.UseCompatibleTextRendering = true;
+            this.btSpin.UseVisualStyleBackColor = false;
+            this.btSpin.Click += new System.EventHandler(this.btSpin_Click);
+            // 
+            // tmrSpin
+            // 
+            this.tmrSpin.Tick += new System.EventHandler(this.tmrSpin_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::StarBet777.Properties.Resources.f1ukeu6xoaazlv8;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btSpin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btSpin;
+        private System.Windows.Forms.Timer tmrSpin;
+    }
+}
+
---
+++ StarBet777/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StarBet777
+{
+    public partial class Form1 : Form
+    {
+        int[] roleta;
+        int[] ciclos;
+        Label[] tela;
+        Random r;
+        public Form1()
+        {
+            InitializeComponent();
+            roleta = new int[3];
+            ciclos = new int[3];
+            tela = new Label[] { label1, label2, label3 };
+            r = new Random();
+            for (int i = 0; i < roleta.Length; i++)
+            {
+                roleta[i] = r.Next(0, 10);
+                Update(i);
+            }
+        }
+
+        void Update(int i)
+        {
+            tela[i].Text = roleta[i].ToString();
+        }
+
+        private void btSpin_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ciclos.Length; i++)
+            {
+                ciclos[i] = r.Next(1, 21);
+                tela[i].ForeColor = Color.Black;
+            }
+            btSpin.Enabled = false;
+            tmrSpin.Enabled = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tmrSpin_Tick(object sender, EventArgs e)
+        {
+            bool stop = true;
+            for (int i = 0; i < ciclos.Length; i++)
+            {
+                if (ciclos[i] > 0)
+                {
+                    ciclos[i]--;
+                    roleta[i]++;
+                    if (ciclos[i] == 0)
+                    {
+                        tela[i].ForeColor = Color.Red;
+                    }
+                    if (roleta[i] == 10)
+                    {
+                        roleta[i] = 0;
+                    }
+                    Update(i);
+                    stop &= false;
+                }
+                else
+                {
+                    stop &= true;
+                }
+            }
+            if (stop)
+            {
+                tmrSpin.Enabled = false;
+                btSpin.Enabled = true;
+                if (roleta[0] == roleta[1] && roleta[1] == roleta[2])
+                {
+                    MessageBox.Show("Você ganhou!");
+                }
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show()
+        }
+    }
+}
